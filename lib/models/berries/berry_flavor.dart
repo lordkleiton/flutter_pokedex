a6774174cacd71bace37836a181a949b392717f7
lordkleiton <m_s_r@outlook.com>
2020-06-19 22:42:08 -0300

    berries finalizado

---
+++ lib/models/berries/berry_flavor.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_pokedex/models/berries/flavor_berry_map.dart';
+import 'package:flutter_pokedex/models/common/name.dart';
+import 'package:flutter_pokedex/models/common/named_api_resource.dart';
+
+class BerryFlavor {
+  int id;
+  String name;
+  List<FlavorBerryMap> berries;
+  NamedApiResource contestType;
+  List<Name> names;
+
+  BerryFlavor.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        name = json['name'],
+        berries = FlavorBerryMap.fromList(json['berries']),
+        names = Name.fromList(json['names']);
+
+  @override
+  toString() =>
+      '$runtimeType {id: $id, name: $name, berries: $berries, contestType: $contestType, names: $names}';
+}
