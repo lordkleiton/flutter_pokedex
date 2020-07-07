ff58c195062b12db30fbd8a8d32fb9e9fe74a4ec
lordkleiton <m_s_r@outlook.com>
2020-07-06 23:54:06 -0300

    class -> mixin

--- lib/state/app_state_pokemon.dart
+++ lib/state/app_state_pokemon.dart
@@ -1,23 +1,19 @@
 import 'package:flutter_pokedex/utils/query.dart';
 import 'package:pokeapi_dart_lib/pokeapi_dart_lib.dart';
 
-class AppStatePokemon {
-  List<Pokemon> pokemons = [];
+mixin AppStatePokemon {
+  List<Pokemon> _pokemons = [];
 
-  List<Pokemon> get pokemon => pokemons;
-
-  void _addPokemon(Pokemon value) {
-    pokemons.add(value);
-  }
+  List<Pokemon> get pokemons => _pokemons;
 
   Future<Pokemon> getPokemon(String query) async {
     final int id = QueryUtils.toId(query);
-    Pokemon result = pokemons.firstWhere((p) => p.name == query || p.id == id,
+    Pokemon result = _pokemons.firstWhere((p) => p.name == query || p.id == id,
         orElse: () => null);
 
     if (result == null) {
       result = await Pokemon.get(QueryUtils.toQueryable(query));
-      _addPokemon(result);
+      _pokemons.add(result);
     }
 
     return result;
