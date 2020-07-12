2cef75a20b4aaf3aff7ec7c014a0505eaaf0296d
lordkleiton <m_s_r@outlook.com>
2020-07-12 01:20:15 -0300

    separando widgets pra eviar jank

---
+++ lib/components/pokemon/pokemon_description_box.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pokedex/state/app_state_species.dart';
+import 'package:pokeapi_dart_lib/pokeapi_dart_lib.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_pokedex/utils/extensions.dart';
+
+class PokemonDescriptionBoxWidget extends StatelessWidget {
+  final Pokemon pokemon;
+
+  PokemonDescriptionBoxWidget({Key key, @required this.pokemon})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final AppStateSpecies state = Provider.of(context);
+    final PokemonSpecies species = state.get(pokemon.species.url);
+    final List<Widget> children = species != null
+        ? species.flavorTextEntries
+            .where((element) => element.language.name == 'en')
+            .toList()
+            .map((element) {
+            return Text(element.flavorText.replaceAll('\n', ' ') +
+                ' - Pokémon ' +
+                element.version.name.capitalize().eliminateDashes());
+          }).toList()
+        : [];
+
+    return Container(
+      height: 200,
+      child: ListView.builder(
+        itemCount: children.length,
+        itemBuilder: (context, index) => children.elementAt(index),
+      ),
+    );
+  }
+}
--- lib/views/pokemon.dart
+++ lib/views/pokemon.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_pokedex/components/moves/moves_box.dart';
+import 'package:flutter_pokedex/components/pokemon/pokemon_description_box.dart';
 import 'package:flutter_pokedex/components/pokemon/pokemon_sprite_gallery.dart';
-import 'package:flutter_pokedex/state/app_state_species.dart';
 import 'package:flutter_pokedex/utils/extensions.dart';
 import 'package:pokeapi_dart_lib/pokeapi_dart_lib.dart';
-import 'package:provider/provider.dart';
 
 class PokemonView extends StatefulWidget {
   final Pokemon pokemon;
@@ -21,15 +20,6 @@ class _PokemonViewState extends State<PokemonView> {
   Widget build(BuildContext context) {
     final Pokemon pokemon = widget.pokemon;
     final String title = pokemon.name.capitalize().eliminateDashes();
-    final AppStateSpecies appState = Provider.of(context);
-    final PokemonSpecies species = appState.get(pokemon.species.url);
-    final List<Widget> children = species != null
-        ? species.flavorTextEntries.map((element) {
-            return Text(element.flavorText.replaceAll('\n', ' ') +
-                ' - Pokémon ' +
-                element.version.name.capitalize().eliminateDashes());
-          }).toList()
-        : [];
 
     return Scaffold(
       appBar: AppBar(
@@ -44,12 +34,8 @@ class _PokemonViewState extends State<PokemonView> {
                 sprites: pokemon.sprites,
               ),
             ),
-            Container(
-              height: 200,
-              child: ListView.builder(
-                itemCount: children.length,
-                itemBuilder: (context, index) => children.elementAt(index),
-              ),
+            PokemonDescriptionBoxWidget(
+              pokemon: pokemon,
             ),
             MovesBoxWidget(
               pokemon: pokemon,
