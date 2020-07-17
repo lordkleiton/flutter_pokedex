8df50efa0f59b366629cbb06668381568340f231
lordkleiton <m_s_r@outlook.com>
2020-07-16 23:43:09 -0300

    sanitizando o nome do pokemon

--- lib/utils/pokemon.dart
+++ lib/utils/pokemon.dart
@@ -1,6 +1,10 @@
+import 'package:flutter_pokedex/utils/classes/extensions.dart';
 import 'package:pokeapi_dart_lib/pokeapi_dart_lib.dart';
 
 abstract class PokemonUtils {
+  static String name(Pokemon pokemon) =>
+      pokemon.name.capitalize().eliminateDashes();
+
   static Future<NamedApiResourceList> find(Map<String, int> data) async {
     final int limit = data['limit'] ?? 0;
     final int skip = data['skip'] ?? 0;
